cb21ce28bb00c13915510f3c22cb9270e9780e04
Shibam Mohanty <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2025-04-06 18:28:29 +0530

    push

--- models/teamModel.js
+++ models/teamModel.js
@@ -4,11 +4,12 @@ const teamSchema = new mongoose.Schema(
   {
     teamName: { type: String, required: true },
     track: { type: String, required: true },
+     collagename: { type: String, required: true },
     members: [
       {
         name: { type: String, required: true },
         email: { type: String, required: true },
-        collagename: { type: String, required: true },
+       
         contactNo: { type: String, required: true },
         gender: { type: String, required: true },
       },
